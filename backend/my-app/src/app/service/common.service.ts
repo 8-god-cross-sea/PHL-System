ec6d28f122f20759ff07c14f238c364140e7c322
357800013@qq.com <357800013@qq.com>
2018-03-15 17:18:00 +0800

    Complete UserService

---
+++ backend/my-app/src/app/service/common.service.ts
@@ -0,0 +1,5 @@
+export class CommonService {
+    static getUrl(route:string):string{
+        return 'http://private-anon-7385ee9207-phl.apiary-mock.com/'+route;
+    }
+}
--- backend/my-app/src/app/service/user.service.ts
+++ backend/my-app/src/app/service/user.service.ts
@@ -1,10 +1,70 @@
 import {Injectable} from '@angular/core';
-import {User} from '../classes/User';
-import {USERS} from '../mock/user.data';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {CommonService} from './common.service';
+
 
 @Injectable()
 export class UserService {
-    getUsers(): User[] {
-        return USERS;
+    constructor(private http: HttpClient) {
+    }
+
+    /// Create a user
+    create(username: string, password: string): Observable<Object> {
+        let url = CommonService.getUrl('user');
+        let options = {
+            headers: new HttpHeaders({'Content-Type': 'application/json'}),
+            params: {username: username, password: password}
+        };
+        return this.http.post(url, options);
+    }
+
+    /// Get information about user who has logged in
+    get_now(): Observable<Object> {
+        let url = CommonService.getUrl('user');
+        return this.http.get(url);
+    }
+
+    login(username: string, password: string): Observable<Object> {
+        let url = CommonService.getUrl('user/login');
+        let options = {
+            headers: new HttpHeaders({'Content-Type': 'application/json'}),
+            params: {username: username, password: password}
+        };
+        return this.http.post(url, options);
+    }
+
+    logout(): Observable<Object> {
+        let url = CommonService.getUrl('user/logout');
+        let options = {
+            headers: new HttpHeaders({'Content-Type': 'application/json'}),
+        };
+        return this.http.post(url,options);
+    }
+
+    /// Get information about user by user id
+    get_by_id(user_id:string):Observable<Object>{
+        let url = CommonService.getUrl('user/'+user_id);
+        return this.http.get(url);
+    }
+
+    update(user_id:string,username:string,password:string):Observable<Object>{
+        let url = CommonService.getUrl('user/'+user_id);
+        let options = {
+            headers: new HttpHeaders({'Content-Type': 'application/json'}),
+            params: {username: username, password: password}
+        };
+        return this.http.put(url,options);
+    }
+
+    delete(user_id:string):Observable<Object>{
+        let url = CommonService.getUrl('user/'+user_id);
+        return this.http.delete(url);
+    }
+
+    /// Get information about all users
+    list():Observable<Object>{
+        let url = CommonService.getUrl('user/list');
+        return this.http.get(url);
     }
 }
